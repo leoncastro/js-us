a26712f0d13404f44ca72a47db88a6bda80fa99a
leoncastro <leoncastro@users.noreply.github.com>
2016-03-05 22:52:59 +0100

    Update openload.user.js

--- openload.co/openload.user.js
+++ openload.co/openload.user.js
@@ -2,34 +2,38 @@
 // @author       @leoncastro
 // @namespace    https://github.com/leoncastro
 // @name         openload.co
-// @version      0.01
-// @description  Remove ads and timer waits, and show direct download link
+// @version      0.02
+// @description  Remove anti-adblock, ads and timer waits, and show direct download link
 // @icon         https://openload.co/favicon.ico
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @include      /^(?:https?:)?\/\/openload\.co\/*.*/
 // @compatible   firefox+greasemonkey
 // @compatible   chrome+tampermonkey
 // @grant        none
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
-
-if( $('#realdl>a').attr('href') )
-{
- $('#btnView').hide();
- $('#btnDl').hide();
- $('.dlButtonContainer').show();
- $('h3.dlfile.h-method').hide();
- $('.col-md-4.col-centered-sm iframe').remove();
- $('#downloadTimer').hide();
- $('#realdl').show();
- // window.location=$('#realdl>a').attr('href');
-}
-
-setInterval(function (){ $("body>object").remove(); },100);
-
+//
+// @run-at document-start
 window.adblock=false;
 window.adblock2=false;
-window.pos=1;
-window.onclick=function(){};
-document.onclick=function(){};
-document.body.onclick=function(){};
+window.open=function(){};
+//
+// @run-at document-end
+(function($){$(function(){
+ if( $('#realdl>a').attr('href') )
+ {
+  $('#btnView').hide();
+  $('#btnDl').hide();
+  $('.dlButtonContainer').show();
+  $('h3.dlfile.h-method').hide();
+  $('.col-md-4.col-centered-sm iframe').remove();
+  $('#downloadTimer').hide();
+  $('#realdl').show();
+  // window.location=$('#realdl>a').attr('href');
+ }
+ setInterval(function (){ $("body>object").remove(); },100);
+ window.pos=1;
+ window.onclick=function(){};
+ document.onclick=function(){};
+ document.body.onclick=function(){};
+})})(window.jQuery.noConflict(true));
