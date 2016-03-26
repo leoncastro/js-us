d48398678a4d90870cee8b37a2631b54df1ca812
leoncastro <leoncastro@users.noreply.github.com>
2016-03-26 22:13:16 +0100

    Update youtube-stop-autostart.user.js

--- youtube.com/youtube-stop-autostart.user.js
+++ youtube.com/youtube-stop-autostart.user.js
@@ -2,7 +2,7 @@
 // @author       @leoncastro
 // @namespace    https://github.com/leoncastro
 // @name         youtube-stop-autostart
-// @version      0.01
+// @version      0.02
 // @description  Stop autostart videos on youtube
 // @icon         http://s.ytimg.com/yts/img/favicon_48-vfl1s0rGh.png
 // @include      /^(https?:)?\/\/(www\.)?youtube\.com\/*.*/
@@ -11,4 +11,10 @@
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
-document.querySelector('#movie_player').cueVideoByPlayerVars(document.querySelector('#movie_player').getVideoData());
+// v0.01: document.querySelector('#movie_player').cueVideoByPlayerVars(document.querySelector('#movie_player').getVideoData());
+// v0.02:
+function ytStopVideo(i){i&&(i=document.querySelector(i))&&i.cueVideoByPlayerVars(i.getVideoData())}
+if(/^\/(channel|user)\//i.exec(location.pathname)) // '/channel/CHANNEL_ID' or '/user/UserName'
+ (new MutationObserver(function(m){ytStopVideo('#c4-player')})).observe(document.querySelector('#upsell-video'),{childList:true});
+else // '/watch?v=VIDEO_ID'
+ ytStopVideo('#movie_player');
