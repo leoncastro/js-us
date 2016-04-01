ffecdc9598a5993369a7aea3cdcf3c3b7ccbf9e2
leoncastro <leoncastro@users.noreply.github.com>
2016-04-01 20:30:33 +0200

    Update goog-no-reminder.user.js

--- youtube.com/goog-no-reminder.user.js
+++ youtube.com/goog-no-reminder.user.js
@@ -4,11 +4,11 @@
 // @license      Public Domain; http://creativecommons.org/publicdomain/zero/1.0/
 // @namespace    http://greasyfork.org
 // @name         goog-no-reminder
-// @version      0.03
+// @version      0.05
 // @description  Remove "A privacy reminder from Google" permanently
 // @include      /(https?:)?\/\/(www\.)?google\.((com)|((com?\.)?[\w]{2}))\/.*/
 // @include      /(https?:)?\/\/(www\.)?youtube\.com\/.*/
 // @grant        GM_addStyle
 // ==/UserScript==
-GM_addStyle('#cnsh,#cnso,#cnsw,.yt-consent,.yt-dialog-bg,#yt-consent-dialog{display:none!important}');
+GM_addStyle('#cnsh,#cnso,#cnsw,._FDh,._SRg,.yt-consent,.yt-dialog-bg,#yt-consent-dialog{display:none!important}');
 document.getElementById('cnsd').click(); // fire "remind me later"
