594063035b221efacbc1128be90144ec8063b178
leoncastro <leoncastro@users.noreply.github.com>
2016-04-11 05:55:46 +0200

    Update allvid.user.js

--- allvid.ch/allvid.user.js
+++ allvid.ch/allvid.user.js
@@ -15,38 +15,30 @@
 !(function(){
 
  function onready(fn){(document.readyState!='loading')?fn():document.addEventListener('DOMContentLoaded',fn)}
+ function addElem(a,e,i,o)
+ {
+  o=document.createElement(a);
+  for(var u in e)
+   o.setAttribute(u,e[u]);
+  o.innerHTML=i;
+  document.body.appendChild(o);
+ }
 
  onready(function(){
-  var v=document.querySelector('#vplayer');
-  if(v)
+  if(document.querySelector('#vplayer'))
   {
-   var s=document.scripts;
-   if(s.length)
+   var n;try{n=jwplayer().getConfig().sources[0].file;}catch(e){}
+   if(n)
    {
-    for(var k=0,l=s.length;k<l;k++)
-    {
-     if(/^\s*eval\(.*\)\s*$/.exec(s[k].text))
-     {
-      var n;
-      try
-      {
-       var c=eval(s[k].text);
-       if(c)n=c.getConfig().sources[0].file;
-      } catch(e){};
-      if(n)
-      {
-       var o=document.createElement('a');
-       o.setAttribute('href',n);
-       o.innerHTML='<style>body>div{height:90vh!important}body>a{position:absolute;bottom:2.5vh;right:1vw;color:#fff;text-decoration:none}.btn-green{height:10vh;padding:1.5vh 1vw;background-color:#4DE664;text-align:center;font-weight:bold;border-radius:5px}</style><span class="btn-green">Download</span>';
-       document.body.appendChild(o);
-      }
-      v=document.querySelector('#admvpu');v&&v.remove();
-      Array.slice(document.querySelectorAll('body>div+div')).forEach(function(e){e.remove()});
-     }
-     if(s[k].text)s[k].text='';
-    }
+    addElem('style',{},'body>div{height:90vh!important}body>a{position:absolute;bottom:2.5vh;right:1vw;color:#fff;text-decoration:none}.btn-green{height:10vh;padding:1.5vh 1vw;background-color:#4DE664;text-align:center;font-weight:bold;border-radius:5px}');
+    addElem('a',{href:n,target:'_blank',type:'application/octet-stream',download:n.split('/')[3]},'<span class="btn-green">Download</span>');
    }
+   addElem('style',{},'#admvpu,body>div+div{display:none!important;position:absolute!important;width:0!important;height:0!important;z-index:-1!important}');
   }
+  window.onclick=function(){};
+  document.onclick=function(){};
+  document.body.onclick=function(){};
+  window.admvpu=function(){};
  });
 
 })();
