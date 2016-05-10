5b9072d47706324bbbb79948e83e2634b1929c9c
leoncastro <leoncastro@users.noreply.github.com>
2016-05-10 04:49:30 +0200

    Create imgwet-captcha.user.js

---
+++ imgwet.com/imgwet-captcha.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @author       @leoncastro
+// @namespace    https://github.com/leoncastro
+// @name         imgwet-captcha
+// @version      0.01
+// @description  Resolve ImgWet captcha
+// @include      /^(?:https?:)?\/\/(?:www\.)?imgwet\.com\/img-.*/
+// @compatible   firefox+greasemonkey
+// @compatible   chrome+tampermonkey
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+//
+!(function(){
+ var img = document.querySelector('#cformi img[usemap="#captchaareas"]');
+ if(img)
+ {
+  var drw = document.createElement('canvas');
+  drw.width = img.width;
+  drw.height = img.height;
+  drw.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+  for(var i = 0; i < 5; i++)
+  {
+   var pix = drw.getContext('2d').getImageData(i * img.width / 5 + img.width / 10, 4, 1, 1).data;
+   if(pix && !pix[0] && !pix[1] && !pix[2])
+   {
+    document.querySelector('#imgContinuei').value = i;
+    document.querySelector('#cformi').submit();
+   }
+  }
+ }
+})();
