bd05f5d6f84861cef994949137ee3b39f1b81bd6
leoncastro <leoncastro@users.noreply.github.com>
2016-09-29 18:51:00 +0200

    Update openload.user.js

--- openload.co/openload.user.js
+++ openload.co/openload.user.js
@@ -2,8 +2,8 @@
 // @author       @leoncastro
 // @namespace    https://github.com/leoncastro
 // @name         openload.co
-// @version      0.05
-// @description  Remove anti-adblock, ads and timer waits, and show direct download link
+// @version      0.05.1
+// @description  Remove anti-adblock, ads, popups and timer waits, and show direct download link
 // @icon         https://openload.co/favicon.ico
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @include      /^(?:https?:)?\/\/openload\.co\/*.*/
@@ -16,6 +16,7 @@
 // @run-at document-start
 window.adblock=false;
 window.adblock2=false;
+window.turnoff=true;
 window.open=function(){};
 //
 // @run-at document-end
