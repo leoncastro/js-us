453c12b670664cda6f81c55ef5e931606e367fe2
leoncastro <leoncastro@users.noreply.github.com>
2016-10-20 21:18:14 +0200

    Update greasysleazy.user.js

--- greasyfork.org/greasysleazy.user.js
+++ greasyfork.org/greasysleazy.user.js
@@ -4,7 +4,7 @@
 // @name         greasy-sleazy-fork
 // @version      0.01
 // @description  Automatic jump to Sleazyfork when required
-// @icon         https://greasyfork.org/assets/blacklogo96-941a633c971b25c0c9e982ed499b5d8c.png
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAMFBMVEVHcEwdHR0rKytERETZ2dk9PT2FhYWurq5gYGACAgL///+zs7NycnLh4eGUlJRZWVmvelI3AAAACXRSTlMA/vv+I8d7UKSOO7XsAAACWElEQVR4AZXW8XKzIBAEcPcQZd2De/+3/T5FqU1tMt36B5PhN8lROJxeM69LyQaSsFyWeXqfeckgCY8GOBHI6xszF/PmJG1LzSGa0JiXX6eDZsAORFMHgpjXp/mrpaBLfgPRAbk8zAd9cx7pgFI7AJzMr5UsJNmGQH/6XyT+EAsxxFNexUpWfRS39TEybR/FMuZn8rsA6eAtcJJcrwKspu+iGcFvwe1HzYa6vYhwB5qCHi1IwvdRWq4VvYkdbE4oKUxJLVWQJqolpXkHmUhfAv/n70Iu0pqMsg720dqX1MQh6nYEHiL9FexVFNLES2jrSclcEl4A5r6m4Cna1nNg2LVHSJyjZVrBM6hV13zxl5S+6y6xXan26/4o/JOwKd/Bq3DwHoCYwBE3QrU2HwK8pR/HKbWgpwaSjQEC5BABkraHJEQIk5mSdH6QGhHAELuG9vTtQdlk/f/P8wN6oPkQJIk9JNFBpp8AIj3E5nYT4xsusDShAzYQW2PIK4Z4KTpP69diWPShAeSTAFj28/yUR9HPdeZfxLofuD8Im48jfW8OGHv5QfROUxqvmKCv0/JTYO5t7+gIaHECCFWqck9fwhuYynSkUC1VTxV9qkSFKSBxCCR5zFdj6r9AO5BZiMLxGIlTgFXL6N3tBkhcNSgYY5fQbv27oINRdFT//zSpIk4Bm6eROd+ukD4AARlA8BAarfgSD5GxJ9TQC3gv8HQ5DFH4JvZ08S7GhzhIllHv5y+R9Xv9OWsxd8Kvm8SUIi/z+7eTkjyshVoSPJd1+ph5XbKZIZfl4U3mH2+RPKg5glBiAAAAAElFTkSuQmCC
 // @include      /^(https?:)?\/\/(www\.)?greasyfork\.org\/.*/
 // @compatible   firefox+greasemonkey
 // @compatible   chrome+tampermonkey
