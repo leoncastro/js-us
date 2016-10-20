4fe83a5a86351e04ca2995cd61102575630e3310
leoncastro <leoncastro@users.noreply.github.com>
2016-10-20 16:50:04 +0200

    Update goog-no-rewritelink.user.js

--- google.tld/goog-no-rewritelink.user.js
+++ google.tld/goog-no-rewritelink.user.js
@@ -5,6 +5,7 @@
 // @version      0.01
 // @description  Remove link redirection on Google search results.
 // @icon         https://addons.cdn.mozilla.net/user-media/addon_icons/661/661700-64.png
+// @include      /(https?:)?\/\/(www\.)?google\.com\/.*/
 // @include      /(https?:)?\/\/(www\.)?google\.((com)|((com?\.)?[\w]{2}))\/.*/
 // @compatible   firefox+greasemonkey
 // @compatible   chrome+tampermonkey
