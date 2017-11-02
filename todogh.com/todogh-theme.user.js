5f594e3dc871826284030c9d3e02746a9b5a32cd
leoncastro <leoncastro@users.noreply.github.com>
2017-11-02 02:11:21 +0100

    Update todogh-theme.user.js

--- todogh.com/todogh-theme.user.js
+++ todogh.com/todogh-theme.user.js
@@ -10,7 +10,7 @@
 // @compatible   firefox+greasemonkey
 // @compatible   chrome+tampermonkey
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 //
 (function(){
@@ -30,7 +30,8 @@
 .section.header{display:block;position:absolute;left:0;top:0;width:250px;height:60px;margin:0;padding:0}
 .section.navigation{display:block;position:absolute;left:0;top:0;width:calc(100% - 250px);height:60px;margin:0 0 0 250px;padding:0}
 .main-menu>li>a{padding:25px 10px}
-.section.wrapper{top:60px;padding:0}
+.section.wrapper{top:40px;padding:0}
+.section-inner{max-width:98%;}
 body.single .content{width:100%}
 .single .post-content{padding:0}
 .post-header,.adsbygoogle,.sidebar,.section.footer,.section.credits,.todog-adlabel{display:none!important}
@@ -38,9 +39,9 @@ body.single .content{width:100%}
 .page-id-60 .post-content>p>iframe{width:100%!important}
 .page-id-60 .post-content>p{margin-bottom:0}
 /* DIRECTO */
-.page-id-11 .post-content>p[style]{width:50%;margin:0;font-size:80%}
-.page-id-11 .post-content>p[style]~div>p>iframe{float:left;width:50%!important;height:100%!important;min-height:500px}
-.page-id-11 .post-content>p[style]~p{display:none}
+.page-id-11 .post-content>p{width:50%;margin:0;font-size:80%}
+.page-id-11 .post-content>p~div>iframe{float:left;width:50%!important;height:100%!important;min-height:500px}
+.page-id-11 .post-content>p~p{display:none}
 .page-id-11 .post-content>h6{display:none}
 /* TELECINCO */
 .page-id-13 .post-content>div+p+div{ width:50%;margin:0; padding:0;font-size:80%}
